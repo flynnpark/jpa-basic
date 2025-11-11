2772c8b211c7789eeaea28db9e62be9f28cbfc4b
Flynn <wphestiraid@gmail.com>
2025-11-11 23:24:27 +0900

    영속성 관리 - 영속성 컨텍스트 2
    - 트랜잭션을 지원하는 쓰기 지연

--- src/main/java/dev/flynnpark/JpaMain.java
+++ src/main/java/dev/flynnpark/JpaMain.java
@@ -15,17 +15,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member(1L, "John Doe");
-            // 1차 캐시에 저장
-            em.persist(member);
-
-            Member findMember1 = em.find(Member.class, 1L);// 1차 캐시에서 조회
-            Member findMember2 = em.find(Member.class, 2L);// DB에서 조회
-            Member findMember3 = em.find(Member.class, 2L);// 1차 캐시에서 조회
-            System.out.println("findMember1.name = " + findMember1.getName());
-            System.out.println("findMember2.name = " + findMember2.getName());
-            System.out.println("findMember2 == findMember3 = " + (findMember2 == findMember3));
-
+            Member memberA = new Member(3L, "Alice");
+            Member memberB = new Member(4L, "Bob");
+            em.persist(memberA);
+            em.persist(memberB);
+            // 여기까지 INSERT SQL을 실행하지 않음
+            // 커밋하는 순간 INSERT SQL 실행
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
